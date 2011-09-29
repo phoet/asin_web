26694bc89c4b60c11abc4db57e8796458f248185
phoet <phoetmail@googlemail.com>
2011-09-29 17:20:17 +0200

    move examples to subfolders and generalize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def cite(text)
     content_tag(:cite, "\"#{text}\"")
   end
+
+  def section(key)
+    key = key.to_s
+    content_tag(:section, content_tag(:h2, "#{key.capitalize}:") + render("home/examples/#{key}", key: key), id: key)
+  end
+
+  def highlight(file)
+    CodeRay.highlight_file(Rails.root.join(file)).html_safe
+  end
 end
