c5479816d016cfe3a3e01b20ef350372709ef45a
Peter Schröder <phoetmail@googlemail.com>
2012-11-07 23:13:24 +0100

    try rails4

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
-source "http://rubygems.org"
+source :rubygems
+ruby "1.9.3"
 
-gem "rails",        "~> 3.2"
+gem "rails",                            github: "rails/rails"
+gem "activerecord-deprecated_finders",  github: "rails/activerecord-deprecated_finders"
+gem "journey",                          github: "rails/journey"
+gem "sprockets-rails",                  github: "rails/sprockets-rails"
 
 gem "asin",         "~> 1.0"
 gem "httpclient",   "~> 2.2"
@@ -12,7 +16,6 @@ gem "thin",         "~> 1.4"
 gem "foreman",      "~> 0.51"
 
 group :assets do
-  gem "compass-rails",  "~> 1.0"
-  gem "sass-rails",     "~> 3.2"
+  gem "sass-rails",     github: "rails/sass-rails"
   gem "uglifier",       "~> 1.2"
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AsinWeb::Application.config.secret_token = 'b7a072317b09e57d4ae4df6aade574d20448eb2c59d47956d2204fcb8a92468ac7d726e68e2202995b6a268bef6a1a6f09fc33782985ce8662a92bbd65219509'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+AsinWeb::Application.config.secret_token = 'cbd8f4e80fa5e605606e60e1c385cf2f6bd920c791775e29c9f2c34b118074b8adcbe424467bc3bcbcd4ecd756d5c9fa5870518e95432f672253091c430b2e33'
