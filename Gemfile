5390aa41315317dd0e6722adc00675304d81aec8
phoet <phoetmail@googlemail.com>
2016-08-22 22:19:50 +0200

    update to rails 5

--- Gemfile
+++ Gemfile
@@ -1,17 +1,14 @@
 source "http://rubygems.org"
 ruby "2.3.0"
 
-gem "rails",    "4.2.5.1"
+gem "rails",    "~> 5.0"
+gem "puma",     "~> 3.6"
 
 gem "asin",     "~> 2.0"
 gem "curb",     "~> 0.8"
 
 gem "haml",     "~> 4.0"
 gem "rouge",    "~> 1.3"
-gem "thin",     "~> 1.6"
-gem "foreman",  "~> 0.78"
 
-group :assets do
-  gem "sass-rails", "~> 4.0"
-  gem "uglifier",   "~> 2.1"
-end
+gem "sass-rails", "~> 5.0"
+gem "uglifier",   "~> 3.0"
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require "rails"
 require "action_controller/railtie"
@@ -6,20 +6,12 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module AsinWeb
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
