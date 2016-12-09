831dfa0a6b99764c60bf0ce4dde8a87cf0a88e90
phoet <phoetmail@googlemail.com>
2016-12-09 20:45:58 +0100

    doit

--- app/assets/javascripts/channels/search.js
+++ app/assets/javascripts/channels/search.js
@@ -1,13 +1,17 @@
 App.search = App.cable.subscriptions.create("SearchChannel", {
   connected: function() {
+    console.log('connected');
     // Called when the subscription is ready for use on the server
   },
 
   disconnected: function() {
+    console.log('disconnected');
     // Called when the subscription has been terminated by the server
   },
 
   received: function(data) {
+    console.log('received');
+    console.table(data);
     // Called when there's incoming data on the websocket for this channel
   }
 });
