a64b2606d15fe9a8b097d8a0451497d95b2c0e56
phoet <phoetmail@googlemail.com>
2017-10-24 14:09:59 +0200

    set asin host

--- config/initializers/asin.rb
+++ config/initializers/asin.rb
@@ -2,6 +2,7 @@ ASIN::Configuration.configure do |config|
   config.secret         = ENV['ASIN_SECRET']
   config.key            = ENV['ASIN_KEY']
   config.associate_tag  = ENV['ASIN_TAG']
+  config.host           = ENV['ASIN_HOST']
 end
 
 HTTPI.adapter = :curb
