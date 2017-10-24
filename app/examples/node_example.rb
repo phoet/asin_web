c39a586cc635e924dbe2c2cf5320880b93f2c295
phoet <phoetmail@googlemail.com>
2017-10-24 14:21:23 +0200

    usea proper german browsenode

--- app/examples/node_example.rb
+++ app/examples/node_example.rb
@@ -4,7 +4,7 @@ class NodeExample
   end
 
   def node_name
-    node_id = '599826'
+    node_id = '284266' # DVD & Blu-ray, https://www.amazon.de/dvd-blu-ray-filme-3D-vhs-video/b/ref=sd_allcat_dvd_blu?ie=UTF8&node=284266
     @client.browse_node(node_id, ResponseGroup: :TopSellers).first.name
   end
 end
