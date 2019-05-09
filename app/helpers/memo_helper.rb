505a4f88428bbc80a0524e977a32a0b0884054d8
phoet <phoetmail@googlemail.com>
2019-05-09 10:22:36 +0200

    raises an error

--- app/helpers/memo_helper.rb
+++ app/helpers/memo_helper.rb
@@ -1,17 +1,25 @@
 module MemoHelper
   def lookup_title
     Memo::It.memo { LookupExample.new.lookup_item_title }
+  rescue
+    "an error occured: #{$!}"
   end
 
   def search_titles
     Memo::It.memo { SearchExample.new.search_ruby_titles }
+  rescue
+    "an error occured: #{$!}"
   end
 
   def checkout_url
     Memo::It.memo { CartExample.new.checkout_url }
+  rescue
+    "an error occured: #{$!}"
   end
 
   def node_name
     Memo::It.memo { NodeExample.new.node_name }
+  rescue
+    "an error occured: #{$!}"
   end
 end
